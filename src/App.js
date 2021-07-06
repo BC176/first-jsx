60911f50ba49937753f0b4148a00c54d83b36e18
Brian Clark <brian.w.clark176@gmail.com>
2021-07-06 17:04:32 -0600

    First Coding Dojo completed React assignment

--- src/App.js
+++ src/App.js
@@ -5,18 +5,17 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Hello Dojo!</h1>
+        <h3>Things I need to do:</h3>
+        <ol>
+          <li>Learn React</li>
+          <li>Learn JS</li>
+          <li>Learn Node.js</li>
+          <li>Learn jQuery</li>
+          <li>Learn JSX</li>
+          <li>Learn MERN altogether</li>
+          <li>Get new job, win at life</li>
+        </ol>
       </header>
     </div>
   );
